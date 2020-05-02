2937dae0aaaf8d2485b59bdb129de0584c5c8955
Jimmy <ultish@gmail.com>
2020-05-02 20:14:05 +1000

    unique constraint on timeblock date, trackedtaskId

--- src/models/timeBlock.js
+++ src/models/timeBlock.js
@@ -1,11 +1,22 @@
 const timeBlock = (sequelize, DataTypes) => {
-  const TimeBlock = sequelize.define('timeblock', {
-    startTime: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      validate: { notEmpty: true },
+  const TimeBlock = sequelize.define(
+    'timeblock',
+    {
+      startTime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: { notEmpty: true },
+      },
     },
-  });
+    {
+      indexes: [
+        {
+          unique: true,
+          fields: ['startTime', 'trackedtaskId'],
+        },
+      ],
+    }
+  );
 
   TimeBlock.associate = (models) => {
     TimeBlock.belongsTo(models.TrackedTask);
